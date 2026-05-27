8909bdd027f23f13844fcb28a647b8197ee368f7
Priyan Arora <priyanarora22@gmail.com>
2026-05-27 10:50:28 -0300

    created robots.txt for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://priyanarora.com",
+
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap()
+  ],
+
   output: "static",
 
   vite: {
